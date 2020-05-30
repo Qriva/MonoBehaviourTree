1c1f22354d4784edea8fe7a77437a792bc6d4ad8
Maciej Pawlisz <griva199@gmail.com>
2020-05-30 04:09:55 +0200

    Added validation checks to main components

---
+++ Runtime/MBTExecutor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(MonoBehaviourTree))]
+    public class MBTExecutor : MonoBehaviour
+    {
+        public MonoBehaviourTree monoBehaviourTree;
+
+        void Reset()
+        {
+            monoBehaviourTree = GetComponent<MonoBehaviourTree>();
+            OnValidate();
+        }
+
+        void Update()
+        {
+            monoBehaviourTree.Tick();
+        }
+
+        #if UNITY_EDITOR
+        void OnValidate()
+        {
+            if (monoBehaviourTree != null && monoBehaviourTree.parent != null)
+            {
+                monoBehaviourTree = null;
+                Debug.LogWarning("Subtree should not be target of update. Select parent tree instead.", this.gameObject);
+            }
+        }
+        #endif
+    }
+}
