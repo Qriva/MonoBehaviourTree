ff918d097a85b0289c96ea52c2d6bc058106d40d
Maciej Pawlisz <griva199@gmail.com>
2020-05-30 05:53:44 +0200

    Added Example1

---
+++ Assets/MonoBehaviourTree/Example/ExampleAssets/Nodes/MoveTowards.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MBT;
+
+namespace MBTExample
+{
+    [MBTNode("Move Towards")]
+    [AddComponentMenu("")]
+    public class MoveTowards : Leaf
+    {
+        public Vector3Reference targetPosition;
+        public TransformReference transformToMove;
+        public float speed = 0.1f;
+
+        public override NodeResult Execute()
+        {
+            Vector3 target = targetPosition.Get().Value;
+            Transform obj = transformToMove.Get().Value;
+            float dist = Vector3.Distance(target, obj.position);
+            if (dist > 0)
+            {
+                obj.position = Vector3.MoveTowards(
+                    obj.position, 
+                    target, 
+                    (speed > dist)? dist : speed 
+                );
+                return NodeResult.running;
+            }
+            else
+            {
+                return NodeResult.success;
+            }
+        }
+    }
+}
---
+++ Assets/MonoBehaviourTree/Example/ExampleAssets/Nodes/SetRandomPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MBT;
+
+namespace MBTExample
+{
+    [AddComponentMenu("")]
+    [MBTNode("Set Random Position")]
+    public class SetRandomPosition : Leaf
+    {
+        public Bounds bounds;
+        public Vector3Reference blackboardVariable;
+
+        public override NodeResult Execute()
+        {
+            blackboardVariable.Get().Value = new Vector3(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y),
+                Random.Range(bounds.min.z, bounds.max.z)
+            );
+            return NodeResult.success;
+        }
+    }
+}
