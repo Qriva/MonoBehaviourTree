c524b37d6463741c3102b9df3825877b50616aa7
Qriva <griva199@gmail.com>
2020-07-05 03:09:13 +0200

    Added QuaternionVariable and reference

---
+++ Runtime/Variables/QuaternionVariable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    public class QuaternionVariable : Variable<Quaternion>
+    {
+        
+    }
+
+    [System.Serializable]
+    public class QuaternionReference : VariableReference<QuaternionVariable, Quaternion>
+    {
+        public QuaternionReference(VarRefMode mode = VarRefMode.EnableConstant)
+        {
+            SetMode(mode);
+        }
+        
+        public QuaternionReference(Quaternion defaultConstant)
+        {
+            useConstant = true;
+            constantValue = defaultConstant;
+        }
+
+        public Quaternion Value
+        {
+            get
+            {
+                return (useConstant)? constantValue : this.GetVariable().Value;
+            }
+            set
+            {
+                if (useConstant)
+                {
+                    constantValue = value;
+                }
+                else
+                {
+                    this.GetVariable().Value = value;
+                }
+            }
+        }
+    }
+}
