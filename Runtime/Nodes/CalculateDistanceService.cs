dd7ada047e346e5aac80bb83cfcafae482803826
Qriva <griva199@gmail.com>
2020-08-01 19:29:43 +0200

    Reworked UpdateDistanceService to CalculateDistanceService

---
+++ Runtime/Nodes/CalculateDistanceService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode("Services/Calculate Distance Service")]
+    public class CalculateDistanceService : Service
+    {
+        public TransformReference transform1;
+        public TransformReference transform2;
+        public FloatReference variable = new FloatReference(VarRefMode.DisableConstant);
+        
+        public override void Task()
+        {
+            Transform t1 = transform1.Value;
+            Transform t2 = transform2.Value;
+            if (t1 == null || t2 == null)
+            {
+                return;
+            }
+            variable.Value = Vector3.Distance(t1.position, t2.position);
+        }
+    }
+}
