8d4da7e7d8c937a0f051fda050f2af44d8767952
Qriva <griva199@gmail.com>
2020-08-03 06:26:46 +0200

    Added TimeLimit node

---
+++ Assets/MonoBehaviourTree/Source/Runtime/Nodes/TimeLimit.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode("Decorators/Time Limit")]
+    public class TimeLimit : Decorator
+    {
+        public float time = 5f;
+        private Coroutine coroutine;
+        private WaitForSeconds waitForSeconds;
+        private bool limitReached;
+
+        public override void OnAllowInterrupt()
+        {
+            StoreBTState();
+        }
+
+        public override void OnEnter()
+        {
+            // IMPROVEMENT: WaitForSeconds could be initialized in some special node init callback
+            if (waitForSeconds == null)
+            {
+                // Create new WaitForSeconds
+                OnValidate();
+            }
+            // Reset block flag
+            limitReached = false;
+            coroutine = StartCoroutine(ScheduleTimeLimit());
+        }
+
+        public override NodeResult Execute()
+        {
+            Node node = GetChild();
+            if (node == null || limitReached) {
+                return NodeResult.failure;
+            }
+            if (node.status == Status.Success || node.status == Status.Failure) {
+                return new NodeResult(node.status);
+            }
+            return new NodeResult(Status.Running, node);
+        }
+
+        public override void OnExit()
+        {
+            if (coroutine == null)
+            {
+                return;
+            }
+            limitReached = false;
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+
+        private IEnumerator ScheduleTimeLimit()
+        {
+            yield return waitForSeconds;
+            limitReached = true;
+            coroutine = null;
+            TryAbort(Abort.Self);
+        }
+
+        void OnValidate()
+        {
+            waitForSeconds = new WaitForSeconds(time);
+        }
+    }
+}
