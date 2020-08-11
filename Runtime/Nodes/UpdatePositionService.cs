4f156e1b3e442fab452037049996ea4290dc55fc
Qriva <griva199@gmail.com>
2020-08-11 03:30:27 +0200

    Added UpdatePositionService node

---
+++ Runtime/Nodes/UpdatePositionService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode("Services/Update Position Service")]
+    public class UpdatePositionService : Service
+    {
+        public TransformReference sourceTransform;
+        public Vector3Reference position = new Vector3Reference(VarRefMode.DisableConstant);
+
+        public override void Task()
+        {
+            Transform t = sourceTransform.Value;
+            if (t == null)
+            {
+                return;
+            }
+            position.Value = t.position;
+        }
+    }
+}
