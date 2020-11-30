04bc73626160b7e5439ae5c7dd3d05566ccd5b26
Qriva <griva199@gmail.com>
2020-11-30 21:46:22 +0100

    Added random deviation to Wait node

--- Runtime/Nodes/Wait.cs
+++ Runtime/Nodes/Wait.cs
@@ -10,25 +10,41 @@ namespace MBT
     {
         [Tooltip("Wait time in seconds")]
         public FloatReference time = new FloatReference(1f);
+        public float randomDeviation = 0f;
         public bool continueOnRestart = false;
         private float timer;
         
         public override void OnEnter()
         {
             if (!continueOnRestart) {
-                timer = 0;
+                timer = (randomDeviation == 0f)? 0f : Random.Range(-randomDeviation, randomDeviation);
             }
         }
 
         public override NodeResult Execute()
         {
             timer += Time.deltaTime;
-            if (timer > time.Value) {
+            if (timer >= time.Value) {
                 // Reset timer in case continueOnRestart option is active
-                timer = 0;
+                if (continueOnRestart)
+                {
+                    timer = (randomDeviation == 0f)? 0f : Random.Range(-randomDeviation, randomDeviation);
+                }
                 return NodeResult.success;
             }
             return NodeResult.running;
+        }
+
+        void OnValidate()
+        {
+            if (time.isConstant)
+            {
+                randomDeviation = Mathf.Clamp(randomDeviation, 0f, time.GetConstant());
+            }
+            else
+            {
+                randomDeviation = Mathf.Clamp(randomDeviation, 0f, 600f);
+            }
         }
     }
 }
