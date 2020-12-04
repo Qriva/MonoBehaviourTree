93c78e159404d38c367ee7ad6d06608a334259d6
Qriva <griva199@gmail.com>
2020-12-04 16:17:28 +0100

    Added blackboard variable synchronization by replacing

--- Assets/MonoBehaviourTree/Source/Runtime/Blackboard.cs
+++ Assets/MonoBehaviourTree/Source/Runtime/Blackboard.cs
@@ -9,7 +9,10 @@ namespace MBT
     {
         public List<BlackboardVariable> variables = new List<BlackboardVariable>();
         private Dictionary<string, BlackboardVariable> dictionary = new Dictionary<string, BlackboardVariable>();
+        [Tooltip("When set, this blackboard will replace variables with matching names from target parent")]
+        public Blackboard masterBlackboard;
 
+        // IMPROVEMENT: https://docs.unity3d.com/ScriptReference/ISerializationCallbackReceiver.html
         void Awake()
         {
             // Initialize variables by keys
@@ -18,6 +21,35 @@ namespace MBT
             {
                 BlackboardVariable var = variables[i];
                 dictionary.Add(var.key, var);
+            }
+            // Replace variables from master blackboard
+            if (masterBlackboard != null)
+            {
+                List<BlackboardVariable> parentVars = masterBlackboard.variables;
+                for (int i = 0; i < parentVars.Count; i++)
+                {
+                    // Find if there is variable with the same key
+                    BlackboardVariable parentVar = parentVars[i];
+                    if (dictionary.TryGetValue(parentVar.key, out BlackboardVariable currentVar))
+                    {
+                        // Ensure that both of them are the same type
+                        if (currentVar.GetType().IsAssignableFrom(parentVar.GetType()))
+                        {
+                            // There are matching variables, replace current one with master blackboard var
+                            dictionary[parentVar.key] = parentVar;
+                        }
+                        else
+                        {
+                            Debug.LogErrorFormat(this, 
+                                "Blackboard variable key '{0}' cannot be replaced. " + 
+                                "Master blackboard variable of type {1} cannot be assigned to {2}.", 
+                                currentVar.key,
+                                parentVar.GetType().Name,
+                                currentVar.GetType().Name
+                            );
+                        }
+                    }
+                }
             }
         }
 
@@ -46,6 +78,15 @@ namespace MBT
         protected bool HasVariables()
         {
             return variables.Count > 0;
+        }
+
+        void OnValidate()
+        {
+            if (masterBlackboard == this)
+            {
+                Debug.LogWarning("Master blackboard cannot be the same instance.");
+                masterBlackboard = null;
+            }
         }
         #endif
     }
