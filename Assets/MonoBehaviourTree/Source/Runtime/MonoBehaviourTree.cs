461274eb349825646880c1118bac2bcf13e75292
Qriva <griva199@gmail.com>
2020-12-05 16:04:29 +0100

    Removed description from MBT component

--- Assets/MonoBehaviourTree/Source/Runtime/MonoBehaviourTree.cs
+++ Assets/MonoBehaviourTree/Source/Runtime/MonoBehaviourTree.cs
@@ -11,7 +11,6 @@ namespace MBT
     {
         [HideInInspector]
         public Node selectedEditorNode;
-        public string description;
         public bool repeatOnFinish = false;
         public int maxExecutionsPerTick = 1000;
         public MonoBehaviourTree parent;
