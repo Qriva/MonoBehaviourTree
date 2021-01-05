407bdc47b6da17e3e00e137257928a350079e55d
Qriva <griva199@gmail.com>
2021-01-05 20:02:03 +0100

    Added DistanceCondition node

---
+++ Runtime/Nodes/DistanceCondition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode(name = "Conditions/Distance Condition")]
+    public class DistanceCondition : Condition
+    {
+        public Comparator comparator = Comparator.GreaterThan;
+        public FloatReference distance = new FloatReference(10f);
+        [Space]
+        public TransformReference transform1;
+        public TransformReference transform2;
+
+        public override bool Check()
+        {
+            // Squared magnitude is enough to compare distances
+            float sqrMagnitude = (transform1.Value.position - transform2.Value.position).sqrMagnitude;
+            float dist = distance.Value;
+            if (comparator == Comparator.GreaterThan)
+            {
+                return sqrMagnitude > dist * dist;
+            }
+            else
+            {
+                return sqrMagnitude < dist * dist;
+            }
+        }
+
+        public enum Comparator
+        {
+            GreaterThan, LessThan
+        }
+    }
+}
