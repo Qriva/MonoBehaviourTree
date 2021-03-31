443e749503b3210783692c54bd323548e6125778
Qriva <griva199@gmail.com>
2021-03-31 21:06:56 +0200

    Added Set Number node

---
+++ Editor/SetNumberEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MBT;
+
+namespace MBTEditor
+{
+    [CustomEditor(typeof(SetNumber))]
+    public class SetNumberEditor : Editor
+    {
+        SerializedProperty titleProp;
+        SerializedProperty typeProp;
+        SerializedProperty operationProp;
+        SerializedProperty sourceIntProp;
+        SerializedProperty sourceFloatProp;
+        SerializedProperty destinationFloatProp;
+        SerializedProperty destinationIntProp;
+
+        void OnEnable()
+        {
+            titleProp = serializedObject.FindProperty("title");
+            typeProp = serializedObject.FindProperty("type");
+            operationProp = serializedObject.FindProperty("operation");
+            sourceFloatProp = serializedObject.FindProperty("sourceFloat");
+            sourceIntProp = serializedObject.FindProperty("sourceInt");
+            destinationFloatProp = serializedObject.FindProperty("destinationFloat");
+            destinationIntProp = serializedObject.FindProperty("destinationInt");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.PropertyField(titleProp);
+            EditorGUILayout.PropertyField(typeProp);
+            EditorGUILayout.Space();
+            if (typeProp.enumValueIndex == (int)SetNumber.Type.Float)
+            {
+                // Float
+                EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.PropertyField(destinationFloatProp, GUIContent.none);
+                    EditorGUILayout.PropertyField(operationProp, GUIContent.none, GUILayout.MaxWidth(60f));
+                    EditorGUILayout.PropertyField(sourceFloatProp, GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+            else
+            {
+                // Int
+                EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.PropertyField(destinationIntProp, GUIContent.none);
+                    EditorGUILayout.PropertyField(operationProp, GUIContent.none, GUILayout.MaxWidth(60f));
+                    EditorGUILayout.PropertyField(sourceIntProp, GUIContent.none);
+                EditorGUILayout.EndHorizontal();
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
---
+++ Runtime/Nodes/SetNumber.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode(name = "Tasks/Set Number")]
+    public class SetNumber : Leaf
+    {
+        public Type type = Type.Float;
+        public Operation operation = Operation.Set;
+
+        public FloatReference sourceFloat = new FloatReference(0f);
+        public IntReference sourceInt = new IntReference(0);
+
+        public FloatReference destinationFloat = new FloatReference(VarRefMode.DisableConstant);
+        public IntReference destinationInt = new IntReference(VarRefMode.DisableConstant);
+        
+        public override NodeResult Execute()
+        {
+            if (type == Type.Float)
+            {
+                // Set float
+                switch (operation)
+                {
+                    case Operation.Set: destinationFloat.Value = sourceFloat.Value;
+                        break;
+                    case Operation.Add: destinationFloat.Value += sourceFloat.Value;
+                        break;
+                    case Operation.Multiply: destinationFloat.Value *= sourceFloat.Value;
+                        break;
+                }
+            }
+            else
+            {
+                // Set int
+                switch (operation)
+                {
+                    case Operation.Set: destinationInt.Value = sourceInt.Value;
+                        break;
+                    case Operation.Add: destinationInt.Value += sourceInt.Value;
+                        break;
+                    case Operation.Multiply: destinationInt.Value *= sourceInt.Value;
+                        break;
+                }
+            }
+            return NodeResult.success;
+        }
+
+        public override bool IsValid()
+        {
+            switch (type)
+            {
+                case Type.Float: return !(sourceFloat.isInvalid || destinationFloat.isInvalid);
+                case Type.Int: return !(sourceInt.isInvalid || destinationInt.isInvalid);
+                default: return true;
+            }
+        }
+
+        public enum Type
+        {
+            Float, Int
+        }
+
+        public enum Operation
+        {
+            [InspectorName("=")]
+            Set, 
+            [InspectorName("+=")]
+            Add, 
+            [InspectorName("*=")]
+            Multiply
+        }
+    }
+}
