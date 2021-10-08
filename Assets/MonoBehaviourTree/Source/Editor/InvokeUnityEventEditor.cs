5372405809ca15fbd06089e40f8d8ec98329936d
Qriva <griva199@gmail.com>
2021-10-09 00:48:32 +0200

    Added InvokeUnityEvent node

---
+++ Assets/MonoBehaviourTree/Source/Editor/InvokeUnityEventEditor.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MBT;
+using System;
+
+namespace MBTEditor
+{
+    [CustomEditor(typeof(InvokeUnityEvent))]
+    public class InvokeUnityEventEditor : Editor
+    {
+        SerializedProperty titleProp;
+        SerializedProperty typeProp;
+
+        private SerializedProperty transformEventProp;
+        private SerializedProperty gameObjectEventProp;
+        private SerializedProperty floatEventProp;
+        private SerializedProperty intEventProp;
+        private SerializedProperty boolEventProp;
+        private SerializedProperty stringEventProp;
+        private SerializedProperty vector3EventProp;
+        private SerializedProperty vector2EventProp;
+        
+        private SerializedProperty transformReferenceProp;
+        private SerializedProperty gameObjectReferenceProp;
+        private SerializedProperty floatReferenceProp;
+        private SerializedProperty intReferenceProp;
+        private SerializedProperty boolReferenceProp;
+        private SerializedProperty stringReferenceProp;
+        private SerializedProperty vector3ReferenceProp;
+        private SerializedProperty vector2ReferenceProp;
+
+        void OnEnable()
+        {
+            titleProp = serializedObject.FindProperty("title");
+            typeProp = serializedObject.FindProperty("type");
+            
+            transformEventProp = serializedObject.FindProperty("transformEvent");
+            gameObjectEventProp = serializedObject.FindProperty("gameObjectEvent");
+            floatEventProp = serializedObject.FindProperty("floatEvent");
+            intEventProp = serializedObject.FindProperty("intEvent");
+            boolEventProp = serializedObject.FindProperty("boolEvent");
+            stringEventProp = serializedObject.FindProperty("stringEvent");
+            vector3EventProp = serializedObject.FindProperty("vector3Event");
+            vector2EventProp = serializedObject.FindProperty("vector2Event");
+
+            transformReferenceProp = serializedObject.FindProperty("transformReference");
+            gameObjectReferenceProp = serializedObject.FindProperty("gameObjectReference");
+            floatReferenceProp = serializedObject.FindProperty("floatReference");
+            intReferenceProp = serializedObject.FindProperty("intReference");
+            boolReferenceProp = serializedObject.FindProperty("boolReference");
+            stringReferenceProp = serializedObject.FindProperty("stringReference");
+            vector3ReferenceProp = serializedObject.FindProperty("vector3Reference");
+            vector2ReferenceProp = serializedObject.FindProperty("vector2Reference");
+        }
+
+        private static readonly GUIContent variableLabel = new GUIContent("Parameter");
+        private static readonly GUIContent eventLabel = new GUIContent("Event");
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.PropertyField(titleProp);
+            EditorGUILayout.PropertyField(typeProp);
+            EditorGUILayout.Space();
+
+            if (GetSerializedProperties(out SerializedProperty eventProp, out SerializedProperty variableProp))
+            {
+                EditorGUILayout.PropertyField(variableProp, variableLabel);
+                EditorGUILayout.PropertyField(eventProp, eventLabel);
+            }
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        private bool GetSerializedProperties(out SerializedProperty eventProp, out SerializedProperty referenceProp)
+        {
+            InvokeUnityEvent.EventType eventType = (InvokeUnityEvent.EventType)typeProp.enumValueIndex;
+            switch (eventType)
+            {
+                case InvokeUnityEvent.EventType.Transform:
+                    eventProp = transformEventProp;
+                    referenceProp = transformReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.Float:
+                    eventProp = floatEventProp;
+                    referenceProp = floatReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.GameObject:
+                    eventProp = gameObjectEventProp;
+                    referenceProp = gameObjectReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.Int:
+                    eventProp = intEventProp;
+                    referenceProp = intReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.String:
+                    eventProp = stringEventProp;
+                    referenceProp = stringReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.Vector3:
+                    eventProp = vector3EventProp;
+                    referenceProp = vector3ReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.Vector2:
+                    eventProp = vector2EventProp;
+                    referenceProp = vector2ReferenceProp;
+                    return true;
+                case InvokeUnityEvent.EventType.Bool:
+                    eventProp = boolEventProp;
+                    referenceProp = boolReferenceProp;
+                    return true;
+                default:
+                    eventProp = null;
+                    referenceProp = null;
+                    return false;
+            }
+        }
+    }
+}
---
+++ Assets/MonoBehaviourTree/Source/Runtime/Nodes/InvokeUnityEvent.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode(name = "Tasks/Invoke Unity Event")]
+    public class InvokeUnityEvent : Leaf
+    {
+        public EventType type;
+
+        public TransformReference transformReference = new TransformReference();
+        public GameObjectReference gameObjectReference = new GameObjectReference();
+        public FloatReference floatReference = new FloatReference();
+        public IntReference intReference = new IntReference();
+        public BoolReference boolReference = new BoolReference();
+        public StringReference stringReference = new StringReference();
+        public Vector3Reference vector3Reference = new Vector3Reference();
+        public Vector2Reference vector2Reference = new Vector2Reference();
+
+        public TransformEvent  transformEvent;
+        public GameObjectEvent gameObjectEvent;
+        public FloatEvent floatEvent;
+        public IntEvent intEvent;
+        public BoolEvent boolEvent;
+        public StringEvent stringEvent;
+        public Vector3Event vector3Event;
+        public Vector2Event vector2Event;
+
+        public override NodeResult Execute()
+        {
+            switch (type)
+            {
+                case EventType.Transform: transformEvent.Invoke(transformReference.Value);
+                    break;
+                case EventType.Float: floatEvent.Invoke(floatReference.Value);
+                    break;
+                case EventType.Bool: boolEvent.Invoke(boolReference.Value);
+                    break;
+                case EventType.String: stringEvent.Invoke(stringReference.Value);
+                    break;
+                case EventType.Vector3: vector3Event.Invoke(vector3Reference.Value);
+                    break;
+                case EventType.Vector2: vector2Event.Invoke(vector2Reference.Value);
+                    break;
+                case EventType.Int: intEvent.Invoke(intReference.Value);
+                    break;
+                case EventType.GameObject: gameObjectEvent.Invoke(gameObjectReference.Value);
+                    break;
+            }
+            return NodeResult.success;
+        }
+
+        public override bool IsValid()
+        {
+            switch (type)
+            {
+                case EventType.Transform: return !transformReference.isInvalid;
+                case EventType.Float: return !floatReference.isInvalid;
+                case EventType.Bool: return !boolReference.isInvalid;
+                case EventType.String: return !stringReference.isInvalid;
+                case EventType.Vector3: return !vector3Reference.isInvalid;
+                case EventType.Vector2: return !vector2Reference.isInvalid;
+                case EventType.Int: return !intReference.isInvalid;
+                case EventType.GameObject: return !gameObjectReference.isInvalid;
+                default: return true;
+            }
+        }
+
+        public enum EventType
+        {
+            Transform, GameObject, Float, Int, Bool, String, Vector3, Vector2
+        }
+
+        [System.Serializable] public class TransformEvent : UnityEvent<Transform>{}
+        [System.Serializable] public class GameObjectEvent : UnityEvent<GameObject>{}
+        [System.Serializable] public class FloatEvent : UnityEvent<float>{}
+        [System.Serializable] public class IntEvent : UnityEvent<int>{}
+        [System.Serializable] public class BoolEvent : UnityEvent<bool>{}
+        [System.Serializable] public class StringEvent : UnityEvent<string>{}
+        [System.Serializable] public class Vector3Event : UnityEvent<Vector3>{}
+        [System.Serializable] public class Vector2Event : UnityEvent<Vector2>{}
+    }
+}
