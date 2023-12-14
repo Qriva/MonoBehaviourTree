81c78fcfb80a73d9f1d7d3f0290aa5688085073d
Qriva <griva199@gmail.com>
2023-12-14 15:54:22 +0100

    Added random float and int node

---
+++ Assets/MonoBehaviourTree/Source/Runtime/Nodes/RandomFloat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode(name = "Tasks/Random Float")]
+    public class RandomFloat : Leaf
+    {
+        public FloatReference min = new FloatReference(0f);
+        public FloatReference max = new FloatReference(1f);
+        public FloatReference output = new FloatReference(VarRefMode.DisableConstant);
+
+        public override NodeResult Execute()
+        {
+            output.Value = Random.Range(min.Value, max.Value);
+            return NodeResult.success;
+        }
+
+        void OnValidate()
+        {
+            if (min.isConstant && max.isConstant)
+            {
+                min.Value = Mathf.Min(min.GetConstant(), max.GetConstant());
+            }
+        }
+    }
+}
---
+++ Assets/MonoBehaviourTree/Source/Runtime/Nodes/RandomInteger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode(name = "Tasks/Random Integer")]
+    public class RandomInteger : Leaf
+    {
+        public IntReference min = new IntReference(0);
+        public IntReference max = new IntReference(10);
+        public IntReference output = new IntReference(VarRefMode.DisableConstant);
+
+        public override NodeResult Execute()
+        {
+            output.Value = Random.Range(min.Value, max.Value);
+            return NodeResult.success;
+        }
+
+        void OnValidate()
+        {
+            if (min.isConstant && max.isConstant)
+            {
+                min.Value = Mathf.Min(min.GetConstant(), max.GetConstant());
+            }
+        }
+    }
+}
