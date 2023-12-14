ec240a11f6a73f033e422b41561b2a5d1c6c74a3
Qriva <griva199@gmail.com>
2023-12-14 15:55:45 +0100

    Added force result node

---
+++ Runtime/Nodes/ForceResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MBT
+{
+    [AddComponentMenu("")]
+    [MBTNode("Decorators/Force Result")]
+    public class ForceResult : Decorator
+    {
+        [SerializeField] private ForcedResult result = ForcedResult.Success;
+
+        public override NodeResult Execute()
+        {
+            if (!TryGetChild(out Node node))
+            {
+                return NodeResult.failure;
+            }
+
+            if (node.status == Status.Success || node.status == Status.Failure)
+            {
+                return result == ForcedResult.Success ? NodeResult.success : NodeResult.failure;
+            }
+
+            return node.runningNodeResult;
+        }
+
+        private enum ForcedResult
+        {
+            Success, Failure
+        }
+    }
+}
